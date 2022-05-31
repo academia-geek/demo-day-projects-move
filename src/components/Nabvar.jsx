e92202133ff943a357d59b2bee08308c1b650107
Bethsyf <bethsyfalcon@gmail.com>
2022-05-31 05:07:47 -0500

    add nabvar principal

---
+++ src/components/Nabvar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Button, Container, Nav, Navbar } from 'react-bootstrap'
+
+const Nabvar = () => {
+  return (
+    <>  
+  <Navbar expand="lg" bg="light">
+    <Container fluid >
+      <Navbar.Brand href="/">
+        <img
+          alt=""
+          src="https://res.cloudinary.com/dmaviub4l/image/upload/v1653989340/ihrda8sczta1nboafcdq.png"
+          width="180"          
+          className="d-inline-block align-top"
+        />{' '}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse className='justify-content-end'>    
+    <Nav className='d-grid gap-2 d-flex '>
+    <Button href='/login' variant="success">Iniciar Sesión</Button>
+    <Button href='/register' variant="outline-success">Registrarse</Button>
+    </Nav>
+  </Navbar.Collapse>
+    </Container>
+  </Navbar>
+</>
+  )
+}
+
+export default Nabvar
---
+++ src/routes/AppRouter.js
@@ -0,0 +1,11 @@
+import Nabvar from "../components/Nabvar";
+
+function AppRouter() {
+  return (
+    <div>
+      <Nabvar/>
+    </div>
+  );
+}
+
+export default AppRouter;
