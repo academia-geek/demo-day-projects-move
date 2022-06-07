371c96a40fb8173f2e6def30d534597eb6bdd0e6
Luis Mario Franco <lmfg06@gmail.com>
2022-06-07 16:03:36 -0500

    Documentacion Lender

---
+++ server/src/docs/lender.docs.ts
@@ -0,0 +1,18 @@
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      lender:
+ *          type: object
+ *          properties:
+ *              id_lender:
+ *                  type: string
+ *                  description: Id del prestador (Autogenerado)
+ *              cc_user_fk:
+ *                  type: string
+ *                  description: Cedula del prestador (Debe estar registrado como usuario)
+ *              conductor:
+ *                  type: string
+ *                  description: Si el prestador desea ser conductor o no
+ *              active_lender:
+ */
