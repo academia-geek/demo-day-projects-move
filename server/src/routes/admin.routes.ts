1155efa123911311862fb9fccb602674f9741f73
Luis Mario Franco <lmfg06@gmail.com>
2022-06-10 10:49:14 -0500

    FEAT: ver vehiculos rentados segun filtro de fechas

--- server/src/routes/admin.routes.ts
+++ server/src/routes/admin.routes.ts
@@ -6,7 +6,7 @@ import auth from "../firebase/auth"
 import authSchema from "../schemas-joi/users.schemajoi"
 import validator from '../utilities/validator'
 
-import { collectionUsers } from "../services/database.service";
+import { collectionUsers, collectionVehicles } from "../services/database.service";
 
 import { tokenAdmin } from "../firebase/admin.token";
 import userSchema from "../schemas-joi/user.schemajoi";
@@ -47,3 +47,31 @@ adminRouter.get('/info-lender', tokenAdmin, async (req: Request, res: Response) 
         res.status(500).send(error.message);
     }
 })
+
+// Yo como usuario administrador quiero ver el número de carros rentados según un filtro por fechas
+adminRouter.get('/rented-cars/:inicio/:fin', tokenAdmin, async (req: Request, res: Response) => {
+    const { inicio, fin } = req.params;
+    try {
+        const vehicles = await collectionVehicles.vehicles.find().toArray();
+        const vehiclesRented = vehicles.filter(vehicle => {
+            const f_inicio = new Date(vehicle.fecha_disponibilidad.fecha_inicio);
+            const f_fin = new Date(vehicle.fecha_disponibilidad.fecha_fin);
+            const f_inicio_param = new Date(inicio);
+            const f_fin_param = new Date(fin);
+            if (f_inicio_param <= f_inicio && f_fin_param >= f_fin) {
+                if (vehicle.activo === false) {
+                    return vehicle;
+                }
+            }
+        })
+        res.status(200).send({
+            vehiclesRented: vehiclesRented.length,
+            vehicles: vehiclesRented
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+})
+
+// Ver la lista de prestadores y el reporte de sus comisiones
