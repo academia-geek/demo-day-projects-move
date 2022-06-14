8e7529eefe8497237dc23f8a459a47c9e318af89
Bethsyf <bethsyfalcon@gmail.com>
2022-06-14 05:50:53 -0500

    ajustes para visualizar

--- src/components/Vehicles/GamaAlta.jsx
+++ src/components/Vehicles/GamaAlta.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import CardCar from '../CardCar'
 
 const GamaAlta = () => {
   return (
-    <div style={{marginLeft:'300px', height:'100vh', marginRight:'50px'}}></div>
+    <div style={{marginLeft:'300px', height:'100vh', marginRight:'50px'}}>
+      <CardCar/>
+    </div>
   )
 }
 
--- src/components/Vehicles/GamaMedia.jsx
+++ src/components/Vehicles/GamaMedia.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import CardCar from '../CardCar'
 
 const GamaMedia = () => {
   return (
-    <div style={{marginLeft:'300px', height:'100vh', marginRight:'50px'}}></div>
+    <div style={{marginLeft:'300px', height:'100vh', marginRight:'50px'}}>
+      <CardCar/>
+    </div>
   )
 }
 
