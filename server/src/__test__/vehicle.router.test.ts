fdd8a3b0a29d8cc3c9320adcd8aa2a7c9488d694
Luis Mario Franco <lmfg06@gmail.com>
2022-06-18 13:19:38 -0500

    Merge

--- server/src/__test__/vehicle.router.test.ts
+++ server/src/__test__/vehicle.router.test.ts
@@ -2,7 +2,7 @@ import app from "../index";
 import request from "supertest";
 
 describe("POST /vehicle", () => {
-    it("Retorna 200 en la ruta raiz", async () => {
+    it("Retorna 200 en la ruta de los vehiculos", async () => {
         const res = await request(app).get("/vehicles")
         expect(res.statusCode).toEqual(200);
     });
