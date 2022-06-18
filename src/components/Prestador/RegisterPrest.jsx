3977f247de177586803fd9fa0ece886349260e5e
karvaroz <vargasriosk@gmail.com>
2022-06-18 14:27:31 -0500

    registerPrest

--- src/components/Prestador/RegisterPrest.jsx
+++ src/components/Prestador/RegisterPrest.jsx
@@ -1,6 +1,6 @@
-import React, { useRef, useState } from 'react'
-import ReactHTMLDatalist from 'react-html-datalist';
-import Swal from 'sweetalert2';
+import React, { useRef, useState } from "react";
+import ReactHTMLDatalist from "react-html-datalist";
+import Swal from "sweetalert2";
 
 const RegisterPrest = () => {
   const [formValues, setFormValues] = useState();
@@ -15,9 +15,10 @@ const RegisterPrest = () => {
     e.preventDefault();
     console.log(formValues);
     const user = JSON.parse(localStorage.getItem("user")) || "";
+    console.log(user);
     fetch("http://35.211.155.160:5000/auth/users/lender", {
       body: JSON.stringify({
-        cc_user_fk: formValues.cc_user,
+        cc_user_fk: formValues.cc_user_fk,
         conductor: formValues.conductor,
       }),
       headers: {
@@ -27,13 +28,15 @@ const RegisterPrest = () => {
       method: "POST",
     })
       .then((response) => {
-        if (response.status === 200) {
+        console.log(response);
+        if (response.status === 201) {
           Swal.fire("Bien Hecho!", "Registro exitoso", "success");
         } else {
           Swal.fire("Oops...", "Ha ocurrido un error", "error");
         }
       })
       .catch((error) => {
+        console.log(error);
         Swal.fire("Oops...", "Ha ocurrido un error", "error" + error);
       });
     form.current.reset();
@@ -53,15 +56,17 @@ const RegisterPrest = () => {
               placeholder="Tu CC de Identidad"
               onChange={handleChange}
             />
-            <small>¿Quieres ser conductor? </small>
-            <ReactHTMLDatalist
-              name="conductor"
-              onChange={handleChange}
-              options={[
-                { text: "Si", value: "si" },
-                { text: "No", value: "no" },
-              ]}
-            />
+            <div className="text-center">
+              <small>¿Quieres ser conductor? </small>
+              <ReactHTMLDatalist
+                name="conductor"
+                onChange={handleChange}
+                options={[
+                  { text: "Si", value: "si" },
+                  { text: "No", value: "no" },
+                ]}
+              />
+            </div>
             <div className="mt-5 text-center">
               <button className="btn bg-success text-white me-2" type="submit">
                 Registro para Prestador
@@ -72,6 +77,6 @@ const RegisterPrest = () => {
       </div>
     </div>
   );
-}
+};
 
-export default RegisterPrest
+export default RegisterPrest;
